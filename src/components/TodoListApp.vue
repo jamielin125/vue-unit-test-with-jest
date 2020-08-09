c5c675ad11d439158363a4005406a9f1e029bdab
Jamie Lin <jamielin125@alum.ccu.edu.tw>
2020-08-09 22:27:22 +0800

    set up env

---
+++ src/components/TodoListApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="todo-list">
+    todo-list app
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoListApp',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
