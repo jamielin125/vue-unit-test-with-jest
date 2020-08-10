1c22a9478cf66c55a29191970f22efb5f08a12bd
Jamie Lin <jamielin125@alum.ccu.edu.tw>
2020-08-10 21:17:15 +0800

    jest and prettier set up

--- src/components/TodoListApp.vue
+++ src/components/TodoListApp.vue
@@ -8,10 +8,9 @@
 export default {
   name: 'TodoListApp',
   props: {
-    msg: String
-  }
+    msg: String,
+  },
 }
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
